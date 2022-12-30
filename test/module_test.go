70a1068b2e6ca6126091cb5a39841d0763dba02f
steden <1470804@qq.com>
2022-12-30 19:39:06 +0800

    测试覆盖率100%

--- test/module_test.go
+++ test/module_test.go
@@ -3,13 +3,10 @@ package test
 import (
 	"github.com/farseer-go/fs"
 	"github.com/farseer-go/queue"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestModule(t *testing.T) {
 	fs.Initialize[queue.Module]("unit test")
-	assert.Panics(t, func() {
-		fs.Exit(0)
-	})
+	queue.Module{}.Shutdown()
 }
